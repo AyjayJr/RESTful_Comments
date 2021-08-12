16446d3b6d4139d57978c03bf22d44251316a879
AyjayJr <potentshull@gmail.com>
2021-08-12 18:05:43 -0400

    created a form to submit new comments and add them to the index page

--- index.js
+++ index.js
@@ -2,39 +2,55 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// This line parses any data that comes from a url
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// An object that represents a db of comments
 const comments = [ 
   {
-    user: 'Todd',
-    text: 'That is so funny!'
+    username: 'Todd',
+    comment: 'That is so funny!'
 
   },
   {
-    user: 'Anthony',
-    text: 'What am I looking at!?'
+    username: 'Anthony',
+    comment: 'What am I looking at!?'
 
   },
   {
-    user: 'Potent',
-    text: 'Anything is possible.'
+    username: 'Potent',
+    comment: 'Anything is possible.'
 
   },
   {
-    user: 'Richie',
-    text: 'I am extrememly hungry!'
+    username: 'Richie',
+    comment: 'I am extrememly hungry!'
 
   },
 
 ];
 
+// Our main page for our comments resource
 app.get('/comments', (req, res) => {
   // Index is a common term used for the main page of a resource
   // Usually shows all of a certain resource on index
   res.render('comments/index', { comments });  
+})
+
+// Pushes new comment from form on new page to our "database"
+app.post('/comments', (req, res) => {
+	const { username, comment } = req.body;
+	// In the real world this data would be added to a db
+	comments.push({ username, comment });
+	res.send('It worked!');
+})
+
+// A form to create a new comment
+app.get('/comments/new', (req, res) => {
+	res.render('comments/new');
 })
 
 app.get('/tacos', (req, res) => {
