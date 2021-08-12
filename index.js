defde8cf4b0068caefbdd3df4debb3b79288a64d
AyjayJr <potentshull@gmail.com>
2021-08-12 17:47:53 -0400

    created index for comments resource

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const comments = [ 
+  {
+    user: 'Todd',
+    text: 'That is so funny!'
+
+  },
+  {
+    user: 'Anthony',
+    text: 'What am I looking at!?'
+
+  },
+  {
+    user: 'Potent',
+    text: 'Anything is possible.'
+
+  },
+  {
+    user: 'Richie',
+    text: 'I am extrememly hungry!'
+
+  },
+
+];
+
+app.get('/comments', (req, res) => {
+  // Index is a common term used for the main page of a resource
+  // Usually shows all of a certain resource on index
+  res.render('comments/index', { comments });  
+})
+
 app.get('/tacos', (req, res) => {
 	res.send('Get /tacos response');
 })
